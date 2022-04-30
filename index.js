34754ae9305fe23fb4d44f8897aaebb77120ab58
Jared Mates <jaredmates45@gmail.com>
2022-04-29 17:22:35 -0700

    removed .html file extension

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const port = 8080;
 const path = require("path");
 
 // Static Files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    extensions: ["html"],
+  })
+);
 
 app.listen(port, () => {
   console.log(`Example App listening on port ${port}`);
