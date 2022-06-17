2be3947e3c3b7e0f8931ceb4a32d148ed6aee5c9
Jared Mates <jaredmates45@gmail.com>
2022-06-16 19:19:23 -0700

    modified the routing

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ const path = require("path");
 // Static Files
 app.use(
   express.static(path.join(__dirname, "public"), {
-    extensions: ["html"],
+    // extensions: ["html"],
+    // extensions: ["css"],
   })
 );
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/", "index.html"));
+});
+
+app.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/", "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/", "contact-me.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/", "404.html"));
+});
 
 app.listen(port, () => {
   console.log(`Example App listening on port ${port}`);
